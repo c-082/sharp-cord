e026ca172193f0f594b10afba96870a729b99c06
c-082 <251553005+c-082@users.noreply.github.com>
2026-02-19 19:13:16 +0530

    Add pragma directives to suppress CA1822 warning in UtilityService

--- Bot/Services/UtilityService.cs
+++ Bot/Services/UtilityService.cs
@@ -2,5 +2,7 @@ namespace SharpCord.Bot.Services;
 
 public class UtilityService()
 {
+#pragma warning disable CA1822
     public int GetRandomNumber(int maxValue) => Random.Shared.Next(1, maxValue + 1);
+#pragma warning restore CA1822
 }
